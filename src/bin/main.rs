c98017a2856eb4b68b434d36d6f94e37e5a9e326
MarcosAndradeV <mastermarcos1212@hotmail.com>
2026-06-10 10:02:08 -0300

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 use gss::load_gss_from_file;
 
 fn main() {
-    let s = load_gss_from_file("temp/temp.gss");
+    let s = load_gss_from_file("test/test.gss");
     assert!(s.is_ok(), "{s:?}");
     s.unwrap().dump(0);
 }
